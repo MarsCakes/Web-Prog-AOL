04115f75574b5eb135c5630587971509e3297bd6
MarsCakes <imanuel.peter2005@gmail.com>
2025-12-17 00:54:55 +0700

    added a barebones steps and testimonials section to homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,10 +4,10 @@
     <div class="p-5 mb-4 bg-light rounded-3 text-center">
         <div class="container-fluid">
             <h1 class="display-5 fw-bold">
-                WELCOME TO GREEN
+                WELCOME TO GREENMART
             </h1>
             <p class="lead col-lg-8 mx-auto">
-                We are a company that aims to help society reach a greener future by selling recyclable trash.
+                Kami adalah perusahaan yang bertujuan untuk membantu mendorong masyarakat ke masa depan yang lebih hijau.
             </p>
             <p>
                 <a href="#" class="btn btn-primary">
@@ -56,6 +56,70 @@
             <h1 class="display-5 fw-bold">
                 AYO MULAI SEKARANG!
             </h1>
+        </div>
+    </div>
+    <div class="container">
+        <h2 class="pb-2 border-bottom text-center">
+            Cara Penggunaan
+        </h2>
+        <li>
+            <h5>
+                Step 1: Membuat akun GREENMART dan log in
+            </h5>
+            <h5>
+                Step 2: Tekan tombol pesan penjemputan
+            </h5>
+            <h5>
+                Step 3: Mengisi form pemesanan dengan informasi yang tepat
+            </h5>
+            <h5>
+                Step 4: Tekan tombol submit untuk melakukan pemesanan
+            </h5>
+        </li>
+    </div>
+    <div class="container">
+        <h2 class="pb-2 border-bottom text-center">
+            Testimoni
+        </h2>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card border-0 shadow-sm h-100">
+                    <!-- <img src="{{ asset('img/WhatsApp Image 2025-09-16 at 21.46.56_2c253f08.jpg') }}" alt="team_member-1"
+                                            class="card-img-top" /> -->
+                    <div class="card-body text-center">
+                        <h2 class="card-title">
+                            Budi
+                        </h2>
+                        <p class="card-text">
+                            Saya menggunakan GREENMART untuk bisnis saya
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card border-0 shadow-sm h-100">
+                    <div class="card-body text-center">
+                        <h2 class="card-title">
+                            Joni
+                        </h2>
+                        <p class="card-text">
+                            Saya sering menjual recyclables di GREENMART.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card border-0 shadow-sm h-100">
+                    <div class="card-body text-center">
+                        <h2 class="card-title">
+                            Jodi
+                        </h2>
+                        <p class="card-text">
+                            GREENMART sangat berguna untuk mencari bahan-bahan recyclable dengan mudah dan murah
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
